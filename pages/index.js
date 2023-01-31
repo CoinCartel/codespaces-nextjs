161d3369c52be07913a3c042aad8cfe9c6b2a1c9
Zacharie Jon Raymond <arux.tada@gmail.com>
2023-01-31 13:31:26 +0000

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useId,  } from 'react'
 import Button from '../components/Button'
 import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
